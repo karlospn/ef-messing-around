77259a81b40ddf4d233d5349bda0d6b85d22bf64
Carlos Pons <karlospn@gmail.com>
2017-06-10 17:24:19 +0200

    Installed Ninject and created repo

---
+++ Schools/Schools.DB.Infrastructure/Repository/SchoolRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schools.DB.Infrastructure.Context;
+
+namespace Schools.DB.Infrastructure.Repository
+{
+    public class SchoolRepository : ISchoolRepository
+    {
+        private readonly SchoolDBEntities _context;
+
+        public SchoolRepository(SchoolDBEntities context)
+        {
+            _context = context;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return _context.Students
+                .Include("StudentAddress")
+                .Include("Courses")
+                .Include("Standard")
+                .OrderBy( _ => _.StudentID)
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public Task<List<Student>> GetStudentsAsync()
+        {
+            return _context.Students.ToListAsync();
+        }
+
+    }
+}
--- Schools/Schools/Controllers/HomeController.cs
+++ Schools/Schools/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Schools.DB.Infrastructure.Repository;
 
 namespace Schools.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly ISchoolRepository _schoolRepository;
+
+        public HomeController(ISchoolRepository schoolRepository)
         {
-            return View();
+            _schoolRepository = schoolRepository;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            //var students = _schoolRepository.GetStudents();
+            var students = await _schoolRepository.GetStudentsAsync();
+            return View(students);
         }
 
         public ActionResult About()
